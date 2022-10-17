090776c76b5dcdf96c1b34c65db78352065bcb8e
lisssachka <alisek@mail.ru>
2022-10-17 23:27:06 +0400

    Added second task

--- HomeWork2/Program.cs
+++ HomeWork2/Program.cs
@@ -1,6 +1,6 @@
 ﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
-
+/*
 int SecondDigit(int num)
 {
     int desEd = num % 100;
@@ -11,4 +11,35 @@ Console.WriteLine("Enter number");
 int num = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine(SecondDigit(num));
+*/
 
+//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+int Third(int num)
+{
+    int result = 0;
+    while(num >= 1000)
+    {
+        num = num / 10;
+    }
+    if (num < 100) 
+    {
+        result = -1;
+    }
+    else
+    {
+        result = num % 10;
+    }
+    return result;
+}
+Console.WriteLine("Enter number");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int result = Third(num);
+if (result == -1)
+{
+    Console.WriteLine("There is no third digit");
+}
+else
+{
+    Console.WriteLine($"Third digit is {result}");
+}
