cda5a06c31de08133a5384b184dfdc39822366cb
lisssachka <alisek@mail.ru>
2022-10-17 23:25:10 +0400

    Added first task

---
+++ HomeWork2/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+
+int SecondDigit(int num)
+{
+    int desEd = num % 100;
+    int second = desEd / 10;
+    return second;
+}
+Console.WriteLine("Enter number");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(SecondDigit(num));
+
